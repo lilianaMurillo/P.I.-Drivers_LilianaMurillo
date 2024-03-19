b1f6ad988ba21aae4f3b5572d28543d49b671cd5
Liliana M. Murillo <78320736+HomerusLabCode@users.noreply.github.com>
2024-03-19 06:42:53 -0600

    Creacion de modelos 'Driver' y 'Team'

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,11 +1,37 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUID } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
-    name: {
-      type: DataTypes.STRING,
+  sequelize.define("Driver", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    forename: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
   });
---
+++ server/src/models/Team.js
@@ -0,0 +1,17 @@
+const { DataTypes, UUID } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Team", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+    name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        },
+  });
