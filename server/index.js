6620464fc97e15bb427f9cdd0c3470aea110df29
Liliana M. Murillo <78320736+HomerusLabCode@users.noreply.github.com>
2024-03-20 22:44:12 -0600

    cambios finales

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { getTeamsController } = require("./src/controllers/teamsController");
+
 const { conn } = require("./src/db.js");
 const PORT = 3001;
 
 conn
   .sync({ force: true })
   .then(async () => {
-    await getTeamsController();
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
